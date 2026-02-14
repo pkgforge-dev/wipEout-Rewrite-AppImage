5e77572f3087ace02530b39e39cf39c1ee18000d
Link4Electronics <link4electronics@gmail.com>
2026-02-14 06:04:41 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,23 +6,34 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm libdecor
+pacman -Syu --noconfirm \
+    cmake    \
+    glew     \
+    libdecor \
+    sdl2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package wipeout-rewrite-git
-#mkdir -p ./AppDir/bin
-#mv -v /opt/wipeout-rewrite/* ./AppDir/bin
+#make-aur-package
 
 # If the application needs to be manually built that has to be done down here
+echo "Making nightly build of wipEout-Rewrite..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/phoboslab/wipeout-rewrite"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone "$REPO" ./wipEout-Rewrite
+echo "$VERSION" > ~/version
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+cd ./wipEout-Rewrite
+mkdir -p build && cd build
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DPATH_USERDATA="$HOME/"
+make -j$(nproc)
+mv -v wipeout ../../AppDir/bin/wipegame
+wget https://phoboslab.org/files/wipeout-data-v01.zip -O wipeout-data.zip && bsdtar -xfv wipeout-data.zip && rm wipeout-data.zip
+mv -v wipeout ../../AppDir/bin/
