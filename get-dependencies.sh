62f53eb085605925b166614ac3b051d192b0f8e2
Link4Electronics <link4electronics@gmail.com>
2026-08-23 19:45:35 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,14 +7,13 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    cmake    \
-    glew     \
-    libdecor \
+    cmake \
+    glew  \
     sdl2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 echo "Building wipEout-Rewrite..."
 echo "---------------------------------------------------------------"
